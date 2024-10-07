0abc2096d6dc3d4efdc27cf14b952f47308e0045
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-07 21:20:06 +0200

    logging variables

--- main.go
+++ main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+	var myName string = "Jenagan"
+	const mySecondName string = "John"
+	myThirdName := "John"
+
+	fmt.Println(myName)
+	fmt.Println(mySecondName)
+	fmt.Println(myThirdName)
 }
