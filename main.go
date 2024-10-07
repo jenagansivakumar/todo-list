7402983ffe397a693c560c3887f7371868b48c9d
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-07 21:21:34 +0200

    downloaded fiber

--- main.go
+++ main.go
@@ -4,11 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
-	var myName string = "Jenagan"
-	const mySecondName string = "John"
-	myThirdName := "John"
 
-	fmt.Println(myName)
-	fmt.Println(mySecondName)
-	fmt.Println(myThirdName)
 }
