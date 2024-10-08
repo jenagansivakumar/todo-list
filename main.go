d4042920a16fb4f04dbb895d2dbf9e6e06fb4b0f
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-08 22:57:39 +0200

    printing results and product

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello")
+	var printValue string = "Hello"
+	printMe(printValue)
+
+	var num1 int = 1
+	var num2 int = 2
+	var result, product int = practiceNumbers(num1, num2)
+
+	fmt.Println(result, product)
+}
+
+func printMe(printValue string) {
+	fmt.Println(printValue)
+}
+
+func practiceNumbers(num1 int, num2 int) (int, int) {
+	var result int = num1 + num2
+	var product int = num1 * num2
+	return result, product
 }
