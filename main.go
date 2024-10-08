8b3431d5ddb523aeebda091ff703a505fe77ce2b
Jenagan Sivakumar <jenagansivakumar@gmail.com>
2024-10-08 14:52:57 +0200

    created GET route for /

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 	fmt.Println("Hello world")
 	app := fiber.New()
 
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "hello world"})
+	})
+
 	log.Fatal(app.Listen(":4000"))
 
 }
